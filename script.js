0dabc01acd1f50563e51befad1543e6a4d9105d1
Arda Gungor <ardagungor98@gmail.com>
2021-07-26 17:44:15 +0300

    added menu + other improvements

--- script.js
+++ script.js
@@ -1,53 +1,73 @@
+const close = document.querySelector(".close");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const normal = document.querySelector(".normal");
+const bold = document.querySelector(".bold");
+const year = document.querySelector(".year");
+const work = document.querySelector(".work");
+const rect1 = document.getElementById("rect1");
+const rect2 = document.getElementById("rect2");
+const rect3 = document.getElementById("rect3");
+const rect4 = document.getElementById("rect4");
+const body = document.getElementById("body");
+
 document.getElementById("1").addEventListener("click", function () {
-  document.getElementById("rect1").className = "rectangle active";
-  document.getElementById("rect2").className = "rectangle";
-  document.getElementById("rect3").className = "rectangle";
-  document.getElementById("rect4").className = "rectangle";
-  document.getElementById("body").style.backgroundImage =
-    "url(assets/modern-house.png)";
-  document.querySelector(".normal").textContent = "Cennet Koyu";
-  document.querySelector(".bold").textContent = "Modern House";
-  document.querySelector(".year").textContent = "2018";
-  document.querySelector(".work").textContent = "Exterior Design";
+  rect1.className = "rectangle active";
+  rect2.className = "rectangle";
+  rect3.className = "rectangle";
+  rect4.className = "rectangle";
+  body.style.backgroundImage = "url(assets/modern-house.png)";
+  normal.textContent = "Cennet Koyu";
+  bold.textContent = "Modern House";
+  year.textContent = "2018";
+  work.textContent = "Exterior Design";
 });
 
 document.getElementById("2").addEventListener("click", function () {
-  document.getElementById("rect1").className = "rectangle";
-  document.getElementById("rect2").className = "rectangle active";
-  document.getElementById("rect3").className = "rectangle";
-  document.getElementById("rect4").className = "rectangle";
-  document.getElementById("body").style.backgroundImage =
-    "url(assets/swissotel.png)";
-  document.querySelector(".normal").textContent = "SwissOtel";
-  document.querySelector(".bold").textContent = "Bodrum Beach Room";
-  document.querySelector(".year").textContent = "2015";
-  document.querySelector(".work").textContent = "Interior Design";
+  rect1.className = "rectangle";
+  rect2.className = "rectangle active";
+  rect3.className = "rectangle";
+  rect4.className = "rectangle";
+  body.style.backgroundImage = "url(assets/swissotel.png)";
+  normal.textContent = "SwissOtel";
+  bold.textContent = "Bodrum Beach Room";
+  year.textContent = "2015";
+  work.textContent = "Interior Design";
 });
 
 document.getElementById("3").addEventListener("click", function () {
-  setTimeout(function () {
-    document.getElementById("rect1").className = "rectangle";
-    document.getElementById("rect2").className = "rectangle";
-    document.getElementById("rect3").className = "rectangle active";
-    document.getElementById("rect4").className = "rectangle";
-    document.getElementById("body").style.backgroundImage =
-      "url(assets/gundogan.png)";
-    document.querySelector(".normal").textContent = "OXA";
-    document.querySelector(".bold").textContent = "Gündoğan Houses";
-    document.querySelector(".year").textContent = "2017";
-    document.querySelector(".work").textContent = "Interior & Exterior Design";
-  }, 100);
+  rect1.className = "rectangle";
+  rect2.className = "rectangle";
+  rect3.className = "rectangle active";
+  rect4.className = "rectangle";
+  body.style.backgroundImage = "url(assets/gundogan.png)";
+  normal.textContent = "OXA";
+  bold.textContent = "Gündoğan Houses";
+  year.textContent = "2017";
+  work.textContent = "Interior & Exterior Design";
 });
 
 document.getElementById("4").addEventListener("click", function () {
-  document.getElementById("rect1").className = "rectangle";
-  document.getElementById("rect2").className = "rectangle";
-  document.getElementById("rect3").className = "rectangle";
-  document.getElementById("rect4").className = "rectangle active";
-  document.getElementById("body").style.backgroundImage =
-    "url(assets/stone-house.png)";
-  document.querySelector(".normal").textContent = "Cennet Koyu";
-  document.querySelector(".bold").textContent = "Stone House";
-  document.querySelector(".year").textContent = "2018";
-  document.querySelector(".work").textContent = "Exterior Design";
+  rect1.className = "rectangle";
+  rect2.className = "rectangle";
+  rect3.className = "rectangle";
+  rect4.className = "rectangle active";
+  body.style.backgroundImage = "url(assets/stone-house.png)";
+  normal.textContent = "Cennet Koyu";
+  bold.textContent = "Stone House";
+  year.textContent = "2018";
+  work.textContent = "Exterior Design";
+});
+
+hamburger.addEventListener("click", function () {
+  menu.style.display = "block";
+  hamburger.style.display = "none";
+  body.style.overflow = "hidden"
+});
+
+close.addEventListener("click", function () {
+  menu.style.display = "none";
+  hamburger.style.display = "block";
+  body.style.overflowY = "scroll"
+
 });
